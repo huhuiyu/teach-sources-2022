d968efcef6fc3cca41f92903dfdbba9a54cc4ea8
huhuiyu <1069306849@qq.com>
2022-09-13 08:27:13 +0800

    推送多个资源库脚本

---
+++ push.ps1
@@ -0,0 +1,6 @@
+# github链接地址：git remote add github git@github.com:huhuiyu/teach-sources-2022.git
+# jihulab链接地址：git remote add jihulab git@jihulab.com:DarkKnight/teach-sources-2022.git
+# 阿里云链接地址：git remote add aliyun git@codeup.aliyun.com:62e1fa673c1b5281cd478191/teach-sources-2022.git
+git push
+git push jihulab
+git push aliyun
